75e8284df227fb010a7721542dd1083a7c0f047b
sqrwm <tongyu0720@gmail.com>
2025-05-29 16:54:20 +0800

    Update element_util.py

--- utils/element_util.py
+++ utils/element_util.py
@@ -1,4 +1,4 @@
-def extract_element_info(element):
+def extract_element_info(element, max_web_depth=5, web_depth=0, max_web_length=3):
     info = {
         "title": element.window_text(),
         "control_type": element.element_info.control_type,
@@ -31,10 +31,22 @@ def extract_element_info(element):
     except Exception as e:
         pass
 
+    is_web_page = False
     if element.element_info.automation_id == "RootWebArea" and element.element_info.control_type == "Document" \
-        and element.window_text() not in ["Favorites", "Downloads", "History"]:
+        and element.window_text() not in ["Favorites", "Downloads", "History"] :
+        is_web_page = True
+    
+    if web_depth > max_web_depth:
         return info
     
+    next_web_depth = web_depth + 1 if web_depth > 0 else 0
+    if is_web_page and web_depth == 0: 
+        next_web_depth = 1
+
+    idx_web_length = 0
     for child in element.children():
-        info["children"].append(extract_element_info(child))
+        if is_web_page and idx_web_length >= max_web_length:
+            break
+        idx_web_length += 1
+        info["children"].append(extract_element_info(child, max_web_depth=max_web_depth, web_depth=next_web_depth))
     return info
