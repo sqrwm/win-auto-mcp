c819b0b3ce2d21a702bb4e71df1ee53aeaa8580e
sqrwm <tongyu0720@gmail.com>
2025-05-30 18:16:22 +0800

    Update element_util.py

--- utils/element_util.py
+++ utils/element_util.py
@@ -1,4 +1,8 @@
-def extract_element_info(element, max_web_depth=5, web_depth=0, max_web_length=3):
+import time
+
+
+def extract_element_info(element, max_root_depth=6, web_depth=0, max_web_length=5, in_web_page=False):
+    time_s = time.time()
     info = {
         "title": element.window_text(),
         "control_type": element.element_info.control_type,
@@ -31,22 +35,28 @@ def extract_element_info(element, max_web_depth=5, web_depth=0, max_web_length=3
     except Exception as e:
         pass
 
-    is_web_page = False
+    is_web_page_root = False
     if element.element_info.automation_id == "RootWebArea" and element.element_info.control_type == "Document" \
         and element.window_text() not in ["Favorites", "Downloads", "History"] :
-        is_web_page = True
+        is_web_page_root = True
+        in_web_page = True
     
-    if web_depth > max_web_depth:
+    if web_depth >= max_root_depth:
         return info
     
     next_web_depth = web_depth + 1 if web_depth > 0 else 0
-    if is_web_page and web_depth == 0: 
+    if is_web_page_root and web_depth == 0: 
         next_web_depth = 1
 
     idx_web_length = 0
     for child in element.children():
-        if is_web_page and idx_web_length >= max_web_length:
+        if in_web_page and idx_web_length >= max_web_length:
+            break
+        if time.time() - time_s > 10:
             break
         idx_web_length += 1
-        info["children"].append(extract_element_info(child, max_web_depth=max_web_depth, web_depth=next_web_depth))
+        info["children"].append(extract_element_info(child, max_root_depth=max_root_depth, 
+                                                     web_depth=next_web_depth, 
+                                                     max_web_length=max_web_length, 
+                                                     in_web_page=in_web_page))
     return info
