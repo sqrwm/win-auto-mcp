cb7685ac017568d149e6cf0f09e6184b635c9c93
sqrwm <tongyu0720@gmail.com>
2025-06-03 14:45:23 +0800

    Update element_util.py

--- utils/element_util.py
+++ utils/element_util.py
@@ -37,7 +37,8 @@ def extract_element_info(element, max_root_depth=6, web_depth=0, max_web_length=
 
     is_web_page_root = False
     if element.element_info.automation_id == "RootWebArea" and element.element_info.control_type == "Document" \
-        and element.window_text() not in ["Favorites", "Downloads", "History"] :
+        and element.window_text() not in ["Favorites", "Downloads", "History", "Copilot"] \
+        and not element.window_text().startswith("Microsoft Copilot"):
         is_web_page_root = True
         in_web_page = True
     
@@ -52,7 +53,7 @@ def extract_element_info(element, max_root_depth=6, web_depth=0, max_web_length=
     for child in element.children():
         if in_web_page and idx_web_length >= max_web_length:
             break
-        if time.time() - time_s > 10:
+        if time.time() - time_s > 8:
             break
         idx_web_length += 1
         info["children"].append(extract_element_info(child, max_root_depth=max_root_depth, 
